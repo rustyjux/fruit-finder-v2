2ef7aac2ebef93609fa3e2f4990c2715b36f055b
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-02-04 17:10:54 -0800

    set lat long for new tree

--- src/components/AddTree.jsx
+++ src/components/AddTree.jsx
@@ -4,23 +4,32 @@ import { auth, db } from "../utils/firebase";
 
 export default function AddTree() {
     const [newTree, setNewTree] = useState("");
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
     const treeInputRef = useRef(null);
     
-    const treesCollectionRef = collection(db, "trees")
+    const treesCollectionRef = collection(db, "trees");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (newTree === "") return;
+        if (newTree === "" || latitude === "" || longitude === "") return;
 
         await addDoc(treesCollectionRef, {
             description: newTree,
+            location: {
+                latitude: parseFloat(latitude),
+                longitude: parseFloat(longitude),
+            },
             createdAt: serverTimestamp(),
             userDisplayName: auth.currentUser.displayName,
             userEmail: auth.currentUser.email,
         });
 
         setNewTree("");
-    }
+        setLatitude("");
+        setLongitude("");
+    };
+
     return (
         <div className="add-tree-menu">
             <h3>Enter new tree info:</h3>
@@ -31,17 +40,23 @@ export default function AddTree() {
                     placeholder="Add tree details here"
                     onChange={(e) => setNewTree(e.target.value)}
                     value={newTree}
-                />
+                /><br/>
+                <input 
+                    className="latitude-input"
+                    placeholder="Latitude"
+                    onChange={(e) => setLatitude(e.target.value)}
+                    value={latitude}
+                /><br/>
+                <input 
+                    className="longitude-input"
+                    placeholder="Longitude"
+                    onChange={(e) => setLongitude(e.target.value)}
+                    value={longitude}
+                /><br/>
                 <button type="submit" className="submit-button">
                     Submit
                 </button>
             </form>
-            {/* <br/>
-
-            <input ref={treeInputRef} />
-            <button onClick={() => setNewTree(treeInputRef.current.value)}>
-                Submit new tree
-            </button> */}
         </div>
-    )
+    );
 }
