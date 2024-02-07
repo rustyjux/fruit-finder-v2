9f7e486496b78f597e9b7f4d7796fe993f1239dd
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-02-06 20:28:14 -0800

    display trees from firestore

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,9 @@
-import './Map.css';
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { collection, onSnapshot, query, limit } from 'firebase/firestore';
+import { db } from "../../utils/firebase";
+
+import './Map.css';
 import treeData from "../../test-data/tree-data.json";
 import { appleLIcon } from './MapIcons';
 import TreeMarker from './TreeMarker';
@@ -7,7 +11,27 @@ import TreeMarker from './TreeMarker';
 // Get only actual tree points, not comments
 const treeItems = treeData.features.filter(tree => tree.geometry);
 
-export default function Map() {
+export default function Map() {  
+  console.log('map renders')
+  const [trees, setTrees] = useState([])
+  const treesCollectionRef = collection(db, "tree-features");
+
+  useEffect(() => {
+    const queryTrees = query(treesCollectionRef, limit(10));
+    // const queryTrees = query(treesCollectionRef, where("userDisplayName", "==", "Russell Vinegar"));
+    const unsubscribe = onSnapshot(queryTrees, (snapshot) => {
+      let firestoreTrees = [];
+      snapshot.forEach((doc) => {
+        firestoreTrees.push({...doc.data(), id: doc.id });
+        console.log('record update fetched')
+      })
+      setTrees(firestoreTrees);
+      console.log('state updated')
+    });
+
+    return () => unsubscribe();
+  }, [])
+
     return (
         <MapContainer center={[49.076,-117.8023979]} zoom={15} scrollWheelZoom={true}>
         <TileLayer
@@ -15,29 +39,11 @@ export default function Map() {
         //   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           url="https://api.mapbox.com/styles/v1/rustyjux/clny3ijlz003j01mvbr8xeqrc/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicnVzdHlqdXgiLCJhIjoiY2wxNTZ0ZmRxMHBvejNldjBvY2JpYTIzOCJ9.u3wYmClN0txsgeC9KM_HLw"
         />
-
-        {treeItems.map(tree => (
+  
+        {trees.map(tree => (
           <TreeMarker key={tree.id} tree={tree} />
         ))}      
 
-        {treeItems.map(tree => {
-          return (
-            <Marker 
-              // className = {tree.properties.Type}
-              key = {tree.id}
-              position={[ tree.geometry.coordinates[1], tree.geometry.coordinates[0] ]}
-              icon={appleLIcon}
-            >
-              <Popup
-                position={[ tree.geometry.coordinates[1], tree.geometry.coordinates[0] ]}>
-                <div>
-                  <h2>A new tree?</h2>
-                  {/* <h2>{"Tree type: " + tree.properties.Type}</h2> */}
-                </div>
-              </Popup>
-            </Marker>
-          );
-        })}      
         </MapContainer>
     )
 }
--- src/components/Map/TreeMarker.jsx
+++ src/components/Map/TreeMarker.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { CircleMarker, Popup } from 'react-leaflet';
 
 const TreeMarker = ({ tree }) => {
+  console.log(tree)
   // Mapping of tree types to colors
   const typeColorMapping = {
     apple: 'green',
@@ -22,6 +23,8 @@ const TreeMarker = ({ tree }) => {
       <Popup position={[tree.geometry.coordinates[1], tree.geometry.coordinates[0]]}>
         <div>
           <h2>{"Tree type: " + tree.properties.Type}</h2>
+          <div>{"ID: " + tree.id}</div>
+          <button onClick={null} className='change-tree-type'>Make it a banana!</button>
         </div>
       </Popup>
     </CircleMarker>
