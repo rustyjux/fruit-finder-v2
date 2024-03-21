e11650fbc28e8c231774b288d5245e69877f0625
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-03-20 19:21:22 -0700

    add icon for ripe

--- src/components/Map/TreeMarker.jsx
+++ src/components/Map/TreeMarker.jsx
@@ -1,4 +1,4 @@
-import { CircleMarker } from 'react-leaflet';
+import { Marker, CircleMarker } from 'react-leaflet';
 
 export default function TreeMarker ({ tree, activeTree, makeActiveTree }) {
   
@@ -14,22 +14,43 @@ export default function TreeMarker ({ tree, activeTree, makeActiveTree }) {
 
   const isActive = activeTree && activeTree.id === tree.id;
 
+  const appleIcon = new  L.Icon({
+    iconUrl: 'src/assets/red-apple.svg', // Replace 'path/to/star-icon.svg' with the actual path to your SVG file
+    iconSize: [20, 20], // Adjust the size of the icon as needed
+    iconAnchor: [16, 16], // Position the icon anchor at the center
+  });
+
   return (
-    <CircleMarker 
-      center={[tree.location.latitude, tree.location.longitude]}
-      radius={isActive ? 12 : 8}
-      // color={color}
-      pathOptions={{ 
-        color: isActive ? 'yellow' : color,
-        fillOpacity: tree.picked ? 0 : (tree.ripe ? 0.7 : 0.3)
-      }}
-      eventHandlers={{
-        click: (e) => {
-          // console.log('marker clicked', e)
-          makeActiveTree(tree)
-        },
-      }}
-    >
-    </CircleMarker>
+    <>
+      <CircleMarker 
+        center={[tree.location.latitude, tree.location.longitude]}
+        radius={isActive ? 12 : 8}
+        // color={color}
+        pathOptions={{ 
+          color: isActive ? 'yellow' : color,
+          fillOpacity: tree.picked ? 0 : 0.4
+          // fillOpacity: tree.picked ? 0 : (tree.ripe ? 0.7 : 0.3)
+        }}
+        eventHandlers={{ click: (e) => makeActiveTree(tree) }}
+
+        >
+      </CircleMarker>
+      {tree.ripe && (
+        <Marker 
+        position={[tree.location.latitude, tree.location.longitude]} 
+        icon={appleIcon}
+        eventHandlers={{ click: (e) => makeActiveTree(tree) }}
+        >
+        </Marker>
+      )}
+      {/* {tree.treeCount > 1 && (
+        <Marker 
+        position={[tree.location.latitude, tree.location.longitude]} 
+        icon={appleIcon}
+        eventHandlers={{ click: (e) => makeActiveTree(tree) }}
+        >
+        </Marker>
+      )} */}
+    </>
   );
 };
