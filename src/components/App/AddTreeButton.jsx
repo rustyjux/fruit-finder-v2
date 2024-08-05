981bfb36bdf2534e3beaab6c4adf9def828991ac
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-08-05 14:01:19 -0700

    restyle add tree

--- src/components/App/AddTreeButton.jsx
+++ src/components/App/AddTreeButton.jsx
@@ -2,10 +2,10 @@ import { FaPlus } from 'react-icons/fa6';
 
 export default function AddTreeButton({ activeTree, onClick }) {
 
-  const bgColor = activeTree === 'new-tree' ? 'bg-hot' : 'bg-input';
+  const newTree = activeTree === 'new-tree' ? true : false;
 
   return (
-    <div className={`cursor-pointer absolute bottom-3.5 right-3.5 ${bgColor} rounded-full w-16 h-16 flex justify-center items-center text-white z-50 overflow-hidden drop-shadow-md`}
+    <div className={`cursor-pointer absolute bottom-3.5 right-3.5 outline outline-3 ${newTree ? 'outline-primary text-primary':'outline-input text-input'} bg-white rounded-full w-16 h-16 flex justify-center items-center z-50 overflow-hidden drop-shadow-md`}
     onClick={onClick}>
       <FaPlus size={30} />
     </div>
