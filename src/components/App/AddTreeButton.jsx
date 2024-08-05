cc83b3fc5a5dd4d83771947deb2ba8d3a3ccd335
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-08-05 14:20:58 -0700

    fix line width

--- src/components/App/AddTreeButton.jsx
+++ src/components/App/AddTreeButton.jsx
@@ -5,7 +5,7 @@ export default function AddTreeButton({ activeTree, onClick }) {
   const newTree = activeTree === 'new-tree' ? true : false;
 
   return (
-    <div className={`cursor-pointer absolute bottom-3.5 right-3.5 outline outline-3 ${newTree ? 'outline-primary text-primary':'outline-input text-input'} bg-white rounded-full w-16 h-16 flex justify-center items-center z-50 overflow-hidden drop-shadow-md`}
+    <div className={`cursor-pointer absolute bottom-3.5 right-3.5 outline outline-4 ${newTree ? 'outline-primary text-primary':'outline-input text-input'} bg-white rounded-full w-16 h-16 flex justify-center items-center z-50 overflow-hidden drop-shadow-md`}
     onClick={onClick}>
       <FaPlus size={30} />
     </div>
