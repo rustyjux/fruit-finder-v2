4e86fea22c5e16c69c5d6710e1757bce95ee26f3
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-08-05 14:01:10 -0700

    swap icon for filter

--- src/components/App/LegendButton.jsx
+++ src/components/App/LegendButton.jsx
@@ -1,4 +1,4 @@
-import { FaLayerGroup } from 'react-icons/fa';
+import { FaFilter } from 'react-icons/fa';
 
 export default function LegendButton({ isLegendVisible, onClick }) {
 
@@ -7,7 +7,7 @@ export default function LegendButton({ isLegendVisible, onClick }) {
       className={`cursor-pointer absolute inset-y-[62px] left-3.5 ${isLegendVisible ? 'outline-primary text-primary':'outline-input text-input'} bg-white rounded-full w-10 h-10 flex justify-center items-center text-input outline outline-2 z-50 overflow-hidden drop-shadow-md`}
       onClick={onClick}
     >
-      <FaLayerGroup size={17}/>
+      <FaFilter size={18}/>
     </div>
   );
 }
